03f9a1f79b6e1d35c96e58e9f012b7167fddf8fa
vignesh <vigneshs561@gmail.com>
2026-01-12 13:03:49 +0530

    model

--- src_code/transformers/img_caption.py
+++ src_code/transformers/img_caption.py
@@ -4,5 +4,21 @@ from PIL import Image
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
+'''
+Raw Image/Text
+      ↓
+Processor
+      ↓
+Tensors (pixel_values, input_ids)
+      ↓
+BLIP Model
+      ↓
+Generated Tokens
+      ↓
+Processor.decode()
+      ↓
+Human-readable text
+
+'''
 image = Image.open("../test_images/image_1.jpg")
 print("Data"+ image)
